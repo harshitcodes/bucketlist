04047411148febd9cc73064fa0f69fd14669422d
tyagi_harshit <harshit.tyagi@elucidata.io>
2018-08-09 16:17:54 +0530

    add: environment variables

--- run.py
+++ run.py
@@ -2,9 +2,10 @@ import os
 
 from app import create_app
 
-config_name = os.getenv("APP_SETTINGS")
+config_name = "development"
 app = create_app(config_name)
 
+os.system("export SECRET = \"some-very-long-string-of-random-characters-CHANGE-TO-YOUR-LIKING\"")
 
 if __name__ == '__main__':
     app.run()
