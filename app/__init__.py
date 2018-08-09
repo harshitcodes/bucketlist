082b594b763d7c57d7d45026b5dc0c0c780c7eae
tyagi_harshit <harshit.tyagi@elucidata.io>
2018-08-09 15:13:24 +0530

    add: create testfunctionalities

--- app/__init__.py
+++ app/__init__.py
@@ -2,16 +2,36 @@
 
 from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
-from flask import request, jsonify, abort
+from flask import request, jsonify, abort, make_response
+from functools import wraps
+from passlib.apps import custom_app_context as pwd_context
+from flask_httpauth import HTTPBasicAuth
 
 # local import
 from instance.config import app_config
 
 # initialize sql-alchemy
 db = SQLAlchemy()
+auth = HTTPBasicAuth()
+
+
+def required_roles(*roles):
+   def wrapper(f):
+      @wraps(f)
+      def wrapped(*args, **kwargs):
+         if get_current_user_role() not in roles:
+            flash('Authentication error, please check your details and try again','error')
+            return redirect(url_for('index'))
+         return f(*args, **kwargs)
+      return wrapped
+   return wrapper
+ 
+def get_current_user_role():
+   return g.user.role
+
 
 def create_app(config_name):
-    from app.models import Bucketlist
+    from app.models import Bucketlist, User
 
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
@@ -19,70 +39,176 @@ def create_app(config_name):
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
+    # @app.route('/register/', methods=['POST'])
+    # def new_user():
+    #     username = request.data.get('username')
+    #     email = request.data.get('email')        
+    #     print(username)
+    #     password = request.data.get('password')
+    #     pwd_hash = pwd_context.encrypt(password)
+    #     urole = request.data.get('role')
+    #     if username is None or password is None:
+    #         abort(400) # missing arguments
+    #     if User.query.filter_by(username = username).first() is not None:
+    #         abort(400) # existing user
+    #     user = User(username = username, urole=urole, email=email, pwd_hash = pwd_hash)
+    #     db.session.add(user)
+    #     db.session.commit()
+    #     return jsonify({ 'username': user.username,
+    #                      'urole': user.urole }
+    #                 ), 201,{'Location': url_for('get_user', id = user.id, _external = True)}
+
+    # @app.route("/login", methods=["GET", "POST"])
+    # def login():
+    #     if request.method == 'POST':
+    #         username = request.data.get('username')
+    #         password = request.data.get('password')
+    #         if password == username + "_secret":
+    #             id = username.split('user')[1]
+    #             user = User(id)
+    #             login_user(user)
+    #             return redirect(request.args.get("next"))
+    #         else:
+    #             return abort(401)
+    #     else:
+    #         return Response('''
+    #         <form action="" method="post">
+    #             <p><input type=text name=username>
+    #             <p><input type=password name=password>
+    #             <p><input type=submit value=Login>
+    #         </form>
+    #         ''')
+
+
+    @app.route('/customer/<int:id>', methods=['GET'])
+    def new_user():
+        username = request.data.get('username')
+        email = request.data.get('email')
+        password = request.data.get('password')
+        pwd_hash = pwd_context.encrypt(password)
+        urole = request.data.get('role')
+        if username is None or password is None:
+            abort(400) # missing arguments
+        if User.query.filter_by(username = username).first() is not None:
+            abort(400) # existing user
+        user = User(username = username, urole=urole, email=email, pwd_hash = pwd_hash)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({ 'username': user.username, 'urole': user.urole }), 
+        201, {'Location': url_for('get_user', id = user.id, _external = True)}
+
     @app.route('/bucketlists/', methods=['POST', 'GET'])    
     def bucketlists():
-        if request.method == "POST":
-            name = str(request.data.get('name', ''))
-            if name:
-                bucketlist = Bucketlist(name=name)
-                bucketlist.save()
-                response = jsonify({
-                    'id': bucketlist.id,
-                    'name': bucketlist.name,
-                    'date_created': bucketlist.date_created,
-                    'date_modified': bucketlist.date_modified
-                })
-                response.status_code = 201
-                return response
+        # Get the access token from the header
+        auth_header = request.headers.get('Authorization')
+        access_token = auth_header.split(" ")[1]
 
-        else:
-            # get case
-            bucketlists = Bucketlist.get_all()
-            results = []
+        if access_token:
+         # Attempt to decode the token and get the User ID
+            user_id = User.decode_token(access_token)
+            if not isinstance(user_id, str):
+                # Go ahead and handle the request, the user is authenticated
 
-            for bucketlist in bucketlists:
-                obj = {
-                    'id': bucketlist.id,
-                    'name': bucketlist.name,
-                    'date_created': bucketlist.date_created,
-                    'date_modified': bucketlist.date_modified
+                if request.method == "POST":
+                    name = str(request.data.get('name', ''))
+                    if name:
+                        bucketlist = Bucketlist(name=name, created_by=user_id)
+                        bucketlist.save()
+                        response = jsonify({
+                            'id': bucketlist.id,
+                            'name': bucketlist.name,
+                            'date_created': bucketlist.date_created,
+                            'date_modified': bucketlist.date_modified,
+                            'created_by': user_id
+                        })
+
+                        return make_response(response), 201
+
+                else:
+                    # GET all the bucketlists created by this user
+                    bucketlists = Bucketlist.query.filter_by(created_by=user_id)
+                    results = []
+
+                    for bucketlist in bucketlists:
+                        obj = {
+                            'id': bucketlist.id,
+                            'name': bucketlist.name,
+                            'date_created': bucketlist.date_created,
+                            'date_modified': bucketlist.date_modified,
+                            'created_by': bucketlist.created_by
+                        }
+                        results.append(obj)
+
+                    return make_response(jsonify(results)), 200
+            else:
+                # user is not legit, so the payload is an error message
+                message = user_id
+                response = {
+                    'message': message
                 }
-                results.append(obj)
-            response = jsonify(results)
-            response.status_code = 200
-            return response
+                return make_response(jsonify(response)), 401
 
-    @app.route('/bucketlists/<int:id>', methods=['GET','DELETE', 'PUT'])
+    @app.route('/bucketlists/<int:id>', methods=['GET', 'PUT', 'DELETE'])
     def bucketlist_manipulation(id, **kwargs):
-        bucketlist = Bucketlist.query.filter_by(id=id).first()
-        if not bucketlist:
-            abort(404)
-        if request.method == 'PUT':
-            name = str(request.data.get('name', ''))
-            bucketlist.name = name
-            bucketlist.save()
-            response = jsonify({
-                'id': bucketlist.id,
-                'name': bucketlist.name,
-                'date_created': bucketlist.date_created,
-                'date_modified': bucketlist.date_modified
-            })
-            response.status_code = 200
-            return response
-            
-        elif request.method == 'DELETE':
-            bucketlist.delete()
-            return {'message': "Bucketlist {} deleted successfully!".format(bucketlist.id)}, 200
+        # get the access token from the authorization header
+        auth_header = request.headers.get('Authorization')
+        access_token = auth_header.split(" ")[1]
+        if access_token:
+            # Get the user id related to this access token
+            user_id = User.decode_token(access_token)
 
-        else:
-            # GET
-            response = jsonify({
-                'id': bucketlist.id,
-                'name': bucketlist.name,
-                'date_created': bucketlist.date_created,
-                'date_modified': bucketlist.date_modified
-            })
-            response.status_code = 200
-            return response
-            
+            if not isinstance(user_id, str):
+                # If the id is not a string(error), we have a user id
+                # Get the bucketlist with the id specified from the URL (<int:id>)
+                bucketlist = Bucketlist.query.filter_by(id=id).first()
+                if not bucketlist:
+                    # There is no bucketlist with this ID for this User, so
+                    # Raise an HTTPException with a 404 not found status code
+                    abort(404)
+
+                if request.method == "DELETE":
+                    # delete the bucketlist using our delete method
+                    bucketlist.delete()
+                    return {
+                        "message": "bucketlist {} deleted".format(bucketlist.id)
+                    }, 200
+
+                elif request.method == 'PUT':
+                    # Obtain the new name of the bucketlist from the request data
+                    name = str(request.data.get('name', ''))
+
+                    bucketlist.name = name
+                    bucketlist.save()
+
+                    response = {
+                        'id': bucketlist.id,
+                        'name': bucketlist.name,
+                        'date_created': bucketlist.date_created,
+                        'date_modified': bucketlist.date_modified,
+                        'created_by': bucketlist.created_by
+                    }
+                    return make_response(jsonify(response)), 200
+                else:
+                    # Handle GET request, sending back the bucketlist to the user
+                    response = {
+                        'id': bucketlist.id,
+                        'name': bucketlist.name,
+                        'date_created': bucketlist.date_created,
+                        'date_modified': bucketlist.date_modified,
+                        'created_by': bucketlist.created_by
+                    }
+                    return make_response(jsonify(response)), 200
+            else:
+                # user is not legit, so the payload is an error message
+                message = user_id
+                response = {
+                    'message': message
+                }
+                # return an error response, telling the user he is Unauthorized
+                return make_response(jsonify(response)), 401
+
+    # import the authentication blueprint and register it on the app
+    from .auth import auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
     return app
--- app/models.py
+++ app/models.py
@@ -1,6 +1,84 @@
 # app/models.py
 
+import jwt
+import os
+from datetime import datetime, timedelta
+
 from app import db
+from passlib.apps import custom_app_context as pwd_context
+from flask_bcrypt import Bcrypt
+
+class User(db.Model):
+    """This class defines the users table """
+
+    __tablename__ = 'users'
+
+    # Define the columns of the users table, starting with the primary key
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(256), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    urole = db.Column(db.String(80))
+    bucketlists = db.relationship(
+        'Bucketlist', order_by='Bucketlist.id', cascade="all, delete-orphan")
+
+    def __init__(self, email, password, urole):
+        """Initialize the user with an email and a password."""
+        self.email = email
+        self.urole = urole
+        self.password = Bcrypt().generate_password_hash(password).decode()
+
+    def get_urole(self):
+            return self.urole
+
+    def password_is_valid(self, password):
+        """
+        Checks the password against it's hash to validates the user's password
+        """
+        return Bcrypt().check_password_hash(self.password, password)
+
+    def save(self):
+        """Save a user to the database.
+        This includes creating a new user and editing one.
+        """
+        db.session.add(self)
+        db.session.commit()
+
+    def generate_token(self, user_id):
+        """ Generates the access token"""
+
+        try:
+            # set up a payload with an expiration time
+            payload = {
+                'exp': datetime.utcnow() + timedelta(minutes=5),
+                'iat': datetime.utcnow(),
+                'sub': user_id
+            }
+            # create the byte string token using the payload and the SECRET key
+            jwt_string = jwt.encode(
+                payload,
+                os.getenv('SECRET'),
+                algorithm='HS256'
+            )
+            return jwt_string
+
+        except Exception as e:
+            # return an error in string format if an exception occurs
+            return str(e)
+
+    @staticmethod
+    def decode_token(token):
+        """Decodes the access token from the Authorization header."""
+        try:
+            # try to decode the token using our SECRET variable
+            payload = jwt.decode(token, os.getenv('SECRET'))
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            # the token is expired, return an error string
+            return "Expired token. Please login to get a new token"
+        except jwt.InvalidTokenError:
+            # the token is invalid, return an error string
+            return "Invalid token. Please register or login"
+
 
 class Bucketlist(db.Model):
     """This class represents the bucketlist table."""
@@ -13,18 +91,21 @@ class Bucketlist(db.Model):
     date_modified = db.Column(
         db.DateTime, default=db.func.current_timestamp(),
         onupdate=db.func.current_timestamp())
+    created_by = db.Column(db.Integer, db.ForeignKey(User.id))
 
-    def __init__(self, name):
+    def __init__(self, name, created_by):
         """initialize with name."""
         self.name = name
+        self.created_by = created_by
 
     def save(self):
         db.session.add(self)
         db.session.commit()
 
     @staticmethod
-    def get_all():
-        return Bucketlist.query.all()
+    def get_all(user_id):
+        """This method gets all the bucketlists for a given user."""
+        return Bucketlist.query.filter_by(created_by=user_id)
 
     def delete(self):
         db.session.delete(self)
@@ -32,3 +113,4 @@ class Bucketlist(db.Model):
 
     def __repr__(self):
         return "<Bucketlist: {}>".format(self.name)
+
