28013929f509d0db91ec02bbabedf6a6cfe8b41e
tyagi_harshit <harshit.tyagi@elucidata.io>
2018-08-09 16:55:05 +0530

    [update]: run.py for migrations

--- run.py
+++ run.py
@@ -8,4 +8,7 @@ app = create_app(config_name)
 os.system("export SECRET = \"some-very-long-string-of-random-characters-CHANGE-TO-YOUR-LIKING\"")
 
 if __name__ == '__main__':
+    os.system("python manage.py db init")
+    os.system("python manage.py db migrate")
+    os.system("python manage.py db upgrade")
     app.run()
