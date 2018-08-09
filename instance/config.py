c6861c764adee0765aafd81d148288ad328f0ba9
tyagi_harshit <harshit.tyagi@elucidata.io>
2018-08-09 16:45:30 +0530

    update the database uri

--- instance/config.py
+++ instance/config.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     CSRF_ENABLED = True
     SECRET = "some-very-long-string-of-random-characters-CHANGE-TO-YOUR-LIKING"
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/bucketlist_api"
+    SQLALCHEMY_DATABASE_URI = "postgres://otmymtivsqqnpz:caefb80b1694bed72218ec789ac7fb7f918d91af74d633268865caaa0be38e03@ec2-54-197-253-122.compute-1.amazonaws.com:5432/d4qc50r0cimhs8"
 
 class DevelopmentConfig(Config):
     """Configurations for Development."""
